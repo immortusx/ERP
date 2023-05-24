79ff4dda939eaa8c8cfb25fe977d27e8e4006f37
harshil_raval_bk <harshil.raval@balkrushna.com>
2023-05-24 18:18:29 +0530

    branch add and edit functionality

--- server/Routes/branchRoutes.js
+++ server/Routes/branchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { tokenCheck } = require("../Auth/TokenCheck");
 const { db } = require("../Database/dbConfig");
-
+const moment = require('moment')
 const router = express.Router();
 
 
@@ -13,21 +13,65 @@ router.get('/get-branch-data', async (req, res) => {
       console.log({ isSuccess: false, result: err })
       res.send({ isSuccess: false, result: 'err ' })
     } else {
-      const newUrl = `SELECT * FROM users where id != 20  ; `
-      await db.query(newUrl, async (err, getUsers) => {
-        if (err) {
-          console.log({ isSuccess: false, result: err })
-          res.send({ isSuccess: false, result: 'err ' })
-        } else {
-          const myObj = {
-            users: getUsers,
-            branches: getBranchs,
-          }
-          console.log({ isSuccess: true, result: url })
-          res.send({ isSuccess: true, result: myObj })
-        }
+      console.log({ isSuccess: true, result: url })
+      res.send({ isSuccess: true, result: getBranchs })
+    }
+  })
+})
 
-      })
+router.post('/add-new-branch', async (req, res) => {
+  console.log('>>>>>add-new-branch');
+  console.log('req.body', req.body)
+  var cdate = moment().format('YYYY-MM-DD H:m:s');
+  console.log('cdate', cdate)
+
+  const url = `INSERT INTO branches(name, mobile_number,email_id,address,code,create_date,gst_number,description,contact_person) VALUES('${req.body.firmName}','${req.body.mobileNumber}','${req.body.email}','${req.body.address}','${req.body.code}','${cdate}','${req.body.gstNumber}','${req.body.description}','${req.body.contactPerson}')`;
+
+  await db.query(url, async (err, getBranchs) => {
+    console.log('getBranchs', getBranchs)
+    if (err) {
+      console.log({ isSuccess: false, result: err })
+      res.send({ isSuccess: false, result: 'err' })
+    } else {
+
+      console.log({ isSuccess: true, result: url })
+      res.send({ isSuccess: true, result: 'success' })
+    }
+  })
+})
+router.post('/edit-branch-details', async (req, res) => {
+  console.log('>>>>>edit-branch-details');
+  console.log('req.body', req.body)
+  // var cdate = moment().format('YYYY-MM-DD H:m:s');
+  // console.log('cdate', cdate)
+  const url = `UPDATE branches SET name='${req.body.firmName}',mobile_number='${req.body.mobileNumber}',email_id='${req.body.email}',address='${req.body.address}',code='${req.body.code}',gst_number='${req.body.gstNumber}',description='${req.body.description}',contact_person='${req.body.contactPerson}' WHERE id = ${req.body.id}`;
+  // const url = `UPDATE branches (name, mobile_number,email_id,address,code,create_date,gst_number,description) VALUES('${req.body.firmName}','${req.body.mobileNumber}','${req.body.email}','${req.body.address}','${req.body.code}','${cdate}','${req.body.gstNumber}','${req.body.description}')`;
+  console.log('url', url)
+
+  await db.query(url, async (err, updateData) => {
+    if (err) {
+      console.log({ isSuccess: false, result: err })
+      res.send({ isSuccess: false, result: 'err' })
+    } else {
+
+      console.log({ isSuccess: true, result: url })
+      res.send({ isSuccess: true, result: 'success' })
+    }
+  })
+})
+router.get('/delete-branch/:id', async (req, res) => {
+  console.log('>>>>>delete-branch');
+  console.log('req.params', req.params.id)
+  const url = `UPDATE branches SET is_active = '0' WHERE id = '${req.params.id}'`;
+  // console.log('url', url)
+
+  await db.query(url, async (err, updateData) => {
+    if (err) {
+      console.log({ isSuccess: false, result: err })
+      res.send({ isSuccess: false, result: 'err' })
+    } else {
+      console.log({ isSuccess: true, result: url })
+      res.send({ isSuccess: true, result: 'success' })
     }
   })
 })
