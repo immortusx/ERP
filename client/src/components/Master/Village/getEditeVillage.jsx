3d75f53e1aeab606e42afbd472d1c6c2578018fe
DESKTOP-VMNR4DT\DELL <ankita.kantesaria@balkrushna.com>
2023-05-25 14:45:51 +0530

    update and delete village

--- client/src/components/Master/Village/getEditeVillage.jsx
+++ client/src/components/Master/Village/getEditeVillage.jsx
@@ -22,7 +22,7 @@ export const deleteStateAction = async (sData) => {
   }
 };
 
-export const editeVillageAction = async (tData) => {
+export const editeVillageAction = async (vData) => {
   const url = `${process.env.REACT_APP_NODE_URL}/api/master/edit-VillagebyId`;
   const config = {
     headers: {
@@ -31,7 +31,7 @@ export const editeVillageAction = async (tData) => {
   };
 
   try {
-    const response = await Axios.post(url, tData, config); // Send sData as the request payload
+    const response = await Axios.post(url, vData, config); // Send sData as the request payload
     if (response.data?.isSuccess) {
       console.log(response.data, "qwertyuio Village");
       return response.data;
@@ -63,3 +63,44 @@ export const getAllVillageAction = async () => {
     return null;
   }
 }
+export const getVillageById = async (Village_id) => {
+  const url = `${process.env.REACT_APP_NODE_URL}/api/master/get-Villagebyid/${Village_id}`;
+  const config = {
+    headers: {
+      token: localStorage.getItem('rbacToken')
+    }
+  };
+
+  try {
+    const response = await Axios.get(url, config);
+    if (response.data?.isSuccess) {
+      console.log(response.data, "qwertyuio Villageiddd111111");
+      return response.data.result; 
+    }
+    return null
+  } catch (error) {
+    console.error(error);
+    return null; 
+  }
+}
+
+export const deleteVillageAction = async (vData) => {
+  const url = `${process.env.REACT_APP_NODE_URL}/api/master/delete-VillageById`;
+  const config = {
+    headers: {
+      token: localStorage.getItem('rbacToken')
+    }
+  };
+
+  try {
+    const response = await Axios.post(url, vData, config);
+    if (response.data?.isSuccess) {
+      console.log(response.data, "qwertyuio Village");
+      return response.data; 
+    }
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null; 
+  }
+};
