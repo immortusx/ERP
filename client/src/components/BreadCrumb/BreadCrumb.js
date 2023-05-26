fcb23dae94febce6009b9e74b69dff6906546ca6
mukesh-bhuriya <mukesh.bhuriya@balkrushna.com>
2023-05-26 17:01:38 +0530

    Breadcrumbs integration

--- client/src/components/BreadCrumb/BreadCrumb.js
+++ client/src/components/BreadCrumb/BreadCrumb.js
@@ -1,30 +1,35 @@
-import { breadcrumbsClasses } from '@mui/material'
-import React, { useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import HomeIcon from '@mui/icons-material/Home';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
 
-const breadcrumb = {
-    backgroundColor: 'white',
-    border: '1px solid rgba(0, 0, 0.125)',
-    borderRadius: '0.37rem'
-}
 const BreadCrumb = () => {
     const location = useLocation();
-    let currentLink = '';
-    const crumbs = location.pathname.split('/').filter(crumb => crumb !== '').map(crumb => {
-        currentLink += `/${crumb}`
-        return (
-            <div className='crumb' key={crumb}>
-                <Link to={currentLink}>{crumb}</Link>
-            </div>
-            
-        )
-    })
-    console.log(crumbs, 'route');
+    const crumbs = location.pathname
+        .split('/')
+        .filter((crumb) => crumb !== '');
     return (
-            <div className="breadcrumbs" id="breadcrumbs">
-                {crumbs}
-            </div>
-    )
-}
+        <div className='mb-3'>
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                {crumbs.map((crumb, index, array) => {
+                    const currentLink = `/${array.slice(0, index + 1).join('/')}`;
+                    const isLastCrumb = index === array.length - 1;
+                    const isCrumb = array.filter((crumb) => crumb !== 'home');
+                    return (
+                        <Typography key={index} color="textPrimary">
+                            <Link className="crumb-link" to={currentLink} color={isLastCrumb ? 'textPrimary' : 'inherit'}>
+                            {crumb}
+                            </Link>
+                            {index === array.length - 1 && !isLastCrumb && <ExpandMoreIcon fontSize="small" />}
+                        </Typography>
+                    );
+                })}
+            </Breadcrumbs>
+        </div>
+    );
+};
 
-export default BreadCrumb
+export default BreadCrumb;
