e3a34ef58ff3f498f54f9a2c37f5f2ee0839b32f
Balkrushna Web <gen-web-bitbucket@balkrushna.com>
2023-08-10 16:19:07 +0530

    CheckBox and Menu Added

--- client/src/components/Master/Taluka/Taluka_list.jsx
+++ client/src/components/Master/Taluka/Taluka_list.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useMemo, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import Checkbox from '@mui/material/Checkbox'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons/faEllipsisV";
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
@@ -228,16 +230,61 @@ export default function Taluka_list() {
         });
     };
 
+    // const columns = [
+    //     {
+    //         field: 'rowNumber',
+    //         headerAlign: 'center',
+    //         align: 'center',
+    //         headerName: 'No',
+    //         minWidth: 80,
+    //         flex: 1,
+
+    //     },
+
+    const[selectAll,setSelectAll]=useState(false);
+    const[rowData,setRowData]=useState([]);
+    
+    const handleHeaderCheckboxClick=()=>{
+        setSelectAll(!selectAll);
+    }
+  
+    const handleChildCheckboxClick=(itemId)=>{
+        const updatedRowsData=rowData.map((row)=>{
+            if(row.id==itemId){
+                return{
+                    ...row,
+                    checkbox:!row.checkbox,
+                };
+            }
+            return row;
+        });
+        setRowData(updatedRowsData);
+    }
+
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+
     const columns = [
         {
-            field: 'rowNumber',
-            headerAlign: 'center',
-            align: 'center',
-            headerName: 'No',
-            minWidth: 80,
-            flex: 1,
-
-        },
+            field: "rowNumber",
+            headerName: (
+              <Checkbox
+                {...label}
+                checked={selectAll}
+                onClick={handleHeaderCheckboxClick}
+              />
+            ),
+            minWidth: 90,
+            // flex: 1,
+            renderCell: (params) => (
+              <Checkbox
+                {...label}
+                checked={params.row.checkbox}
+                onClick={() => handleChildCheckboxClick(params.row.id)}
+              />
+            ),
+          },
         {
             field: 'Taluka Name',
             headerAlign: 'center',
@@ -284,22 +331,52 @@ export default function Taluka_list() {
             ),
 
         },
+        // {
+        //     field: 'actions',
+        //     headerName: 'Actions',
+        //     className: 'bg-dark',
+        //     sortable: false,
+        //     filterable: false,
+        //     headerAlign: 'center',
+        //     align: 'center',
+        //     disableColumnMenu: true,
+        //     minWidth: 200,
+        //     flex: 1,
+        //     position: 'sticky',
+        //     renderCell: (params) => (
+        //         <div>
+        //             {/* <button onClick={() => { editActionCall(params.row) }} className='myActionBtn m-1'> */}
+        //             <button className='myActionBtn m-1' onClick={() => { editeTalukaModal(params.row) }}>
+        //                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+        //                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+        //                     <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+        //                 </svg>
+        //             </button>
+        //             <button className='myActionBtn m-1' onClick={() => { deleteTalukaAlert(params.row) }}>
+        //                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+        //                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+        //                 </svg>
+        //             </button>
+        //         </div>
+        //     ),
+        // },  
         {
-            field: 'actions',
-            headerName: 'Actions',
-            className: 'bg-dark',
+            field: "menu",
+            headerName: <FontAwesomeIcon icon={faEllipsisV} />,
+            className: "bg-dark",
             sortable: false,
             filterable: false,
-            headerAlign: 'center',
-            align: 'center',
+            headerAlign: "center",
+            align: "center",
             disableColumnMenu: true,
-            minWidth: 200,
-            flex: 1,
-            position: 'sticky',
+            maxWidth: 50,
+            // flex: 1,
+            position: "sticky",
             renderCell: (params) => (
-                <div>
-                    {/* <button onClick={() => { editActionCall(params.row) }} className='myActionBtn m-1'> */}
-                    <button className='myActionBtn m-1' onClick={() => { editeTalukaModal(params.row) }}>
+              <div className="d-flex justify-content-center dotHover">
+                <FontAwesomeIcon icon={faEllipsisV} />
+                <div className="expandDiv">
+                <button className='myActionBtn m-1' onClick={() => { editeTalukaModal(params.row) }}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                             <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -311,10 +388,20 @@ export default function Taluka_list() {
                         </svg>
                     </button>
                 </div>
+              </div>
             ),
-        }
+          },
     ];
-    const rowsData = allTalukaDate.map((item, index) => ({ ...item, rowNumber: index + 1 }));
+
+    useEffect(() => {
+        const rowsData = allTalukaDate.map((item, index) => ({
+          ...item,
+          rowNumber: index + 1,
+          checkbox: selectAll,
+        }));
+        setRowData(rowsData);
+      }, [allTalukaDate, selectAll]);
+    // const rowsData = allTalukaDate.map((item, index) => ({ ...item, rowNumber: index + 1 }));
 
 
     const deleteTalukaActino = () => {
@@ -357,9 +444,9 @@ export default function Taluka_list() {
                 </div>
 
 
-                <div style={{ height: '85vh', width: '100%' }}>
+                <div className='tableMenuHover' style={{ height: '85vh', width: '100%' }}>
                     <DataGrid
-                        rows={rowsData}
+                        rows={rowData}
                         columns={columns}
                         getRowId={(params) => {
                             return params.rowNumber
