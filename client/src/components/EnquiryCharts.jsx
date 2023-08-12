4275a02d3d95624bc4a1869c00a2e7404ed58b64
Mukesh Bhuriya <>
2023-08-12 10:35:25 +0530

    enquiry booking data implmented on charts

--- client/src/components/EnquiryCharts.jsx
+++ client/src/components/EnquiryCharts.jsx
@@ -29,7 +29,7 @@ const EnquiryCharts = () => {
       };
       await axios.get(url, config).then((response) => {
         if (response.data && response.data.isSuccess) {
-          console.log(response.data.result);
+          // console.log(response.data.result);
           setResultData(response.data.result);
         }
       });
@@ -37,81 +37,50 @@ const EnquiryCharts = () => {
     getChartsData();
   }, []);
   useEffect(() => {
-    if (resultData) {
-      resultData.map((item) => {
-        console.log(item.month, "userrrrrrrr");
-        setChartData({
-          labels: [item.month],
-          datasets: [
-            {
-              label: "Enquiries",
-              backgroundColor: "rgba(54, 162, 235, 0.5)",
-              data: [item.totalEnquiry],
-            },
-            {
-              label: "Booking",
-              backgroundColor: "rgba(75, 192, 192, 0.5)",
-              data: [item.totalbooking],
-            },
-          ],
-        });
-      });
+    if (resultData.length > 0) {
+      const updatedLables = resultData.map((item) => item.month);
+      const updateTotalEnquiry = resultData.map((item) => item.totalEnquiry);
+      const updateTotalBooking = resultData.map((item) => item.totalbooking);
+      setChartData((preData) => ({
+        ...preData,
+        labels: updatedLables,
+        datasets: [
+          {
+            label: "Enquiries",
+            backgroundColor: "rgba(54, 162, 235, 0.5)",
+            data: updateTotalEnquiry,
+          },
+          {
+            label: "Booking",
+            backgroundColor: "rgba(75, 192, 192, 0.5)",
+            data: updateTotalBooking,
+          },
+        ],
+      }));
     }
   }, [resultData]);
-  
+
   useEffect(() => {
-    const ctx = document.getElementById("enquiryBookingChart").getContext("2d");
+    if (chartData.labels.length > 0) {
+      const ctx = document
+        .getElementById("enquiryBookingChart")
+        .getContext("2d");
 
-    const data = {
-      labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ],
-      datasets: [
-        {
-          label: "Enquiries",
-          backgroundColor: "rgba(54, 162, 235, 0.5)",
-          data: [10, 20, 15, 30, 25, 12, 18, 22, 28, 14, 21, 27],
-        },
-        {
-          label: "Bookings",
-          backgroundColor: "rgba(75, 192, 192, 0.5)",
-          data: [5, 10, 8, 15, 12, 6, 9, 13, 16, 7, 11, 17],
-        },
-      ],
-    };
-    useEffect(()=> {
-      if(chartData){
-        console.log(chartData,'chartnnbnb')
-        console.log(data,'data')
-        
-      }
-    },[chartData])
-
-    const config = {
-      type: "bar",
-      data: data,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
+      const config = {
+        type: "bar",
+        data: chartData,
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
           },
         },
-      },
-    };
+      };
 
-    new Chart(ctx, config);
-  }, []);
+      new Chart(ctx, config);
+    }
+  }, [chartData]);
 
   return (
     <div className="chart-container">
