4819f0d1a499ee63a8807784a33198b2b8f7b232
karthikv-benagon <127193097+karthikv-benagon@users.noreply.github.com>
2023-09-19 15:05:07 +0530

    lost enquiry changes

--- client/src/components/FollowUp.jsx
+++ client/src/components/FollowUp.jsx
@@ -298,15 +298,86 @@ const [value, setValue] = useState('');
               )}
             </section>
           </div>
-          <div className="d-flex justify-content-center mt-3">
+          <div className=" mt-3">
             <button className="btn btn-primary p-2" type="button">
-             Save Delivery
+              Save Delivery
             </button>
           </div>
         </>
       ) : value === "lost" ? (
-        // Content for invalidenquiry
-        <div>Enquiry</div>
+        <div className=" row mt-4">
+          <section className="d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              Enter Maker
+            </label>
+            <select className="inpClr myInput" name="model">
+              <option value="0" className="myLabel">
+                select Maker
+              </option>
+            </select>
+          </section>
+          <section className="d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              Enter Modal
+            </label>
+            <select className="inpClr myInput" name="model">
+              <option value="0" className="myLabel">
+                select Modal
+              </option>
+            </select>
+          </section>
+          <section className="d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              Enter Varient
+            </label>
+            <select className="inpClr myInput" name="varient">
+              <option value="0" className="myLabel">
+                select Varient
+              </option>
+            </select>
+          </section>
+
+          <section className="d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              Select Commerical Reason 1
+            </label>
+            <select className="inpClr myInput" name="finance">
+              <option value="0" className="myLabel">
+                Select Commerical Reason 1
+              </option>
+            </select>
+          </section>
+          <section className="d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              Select Non-Commerical Reason 2
+            </label>
+            <select className="inpClr myInput" name="bank">
+              <option value="0" className="myLabel">
+                Select Non-Commerical Reason 2
+              </option>
+            </select>
+          </section>
+          <section className="datePicker d-flex mt-3 flex-column col-12 col-sm-6 col-lg-4">
+            <label className="myLabel" htmlFor="email">
+              select Enquiry Lost Date
+            </label>
+            <DatePicker
+              selected={startDate}
+              dateFormat="dd/MM/yyyy"
+              // onChange={(date) =>
+              //   setNewEnquiryData((newEnquiryData) => ({
+              //     ...newEnquiryData,
+              //     ["deliveryDate"]: date,
+              //   }))
+              // }
+            />
+          </section>
+          <div className="mt-3">
+            <button className="btn btn-primary p-2" type="button">
+              Save Lost Enquiry
+            </button>
+          </div>
+        </div>
       ) : value === "invalidenquiry" ? (
         // Content for invalidenquiry
         <div>invalidenquiry</div>
