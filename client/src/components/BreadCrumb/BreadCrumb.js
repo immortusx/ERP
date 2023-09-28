6edc7a88385f2f746b7561116dd883583480d51a
Manan Santoki <manansantoki1702@gmail.com>
2023-09-28 15:44:55 +0530

    add download button and grid breadcrumb

--- client/src/components/BreadCrumb/BreadCrumb.js
+++ client/src/components/BreadCrumb/BreadCrumb.js
@@ -7,36 +7,81 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 
 const BreadCrumb = () => {
-    const location = useLocation();
-    const crumbs = location.pathname
-        .split('/')
-        .filter((crumb) => crumb !== '');
-      console.log(crumbs, "crumbscrumbscrumbscrumbs");
-    return (
-        <div className='mb-3'>
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  const handleDownloadIos = () => {
+    window.alert("App Not Found")
+  };
+
+  const handleDownloadAndroid = () => {
+    const androidDownloadUrl = `${process.env.REACT_APP_NODE_URL}/api/download`;
+    const androidDownloadLink = document.createElement('a');
+    androidDownloadLink.href = androidDownloadUrl;
+    androidDownloadLink.click();
+  };
+  const location = useLocation();
+  const crumbs = location.pathname
+    .split('/')
+    .filter((crumb) => crumb !== '');
+  console.log(crumbs, "crumbscrumbscrumbscrumbs");
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <div className='mb-3'>
+
             <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-                {crumbs.map((crumb, index, array) => {
-                    const currentLink = `/${array.slice(0, index + 1).join('/')}`;
-                    const isLastCrumb = index === array.length - 1;
-                    const isCrumb = array.filter((crumb) => crumb !== 'home');
-                    return (
-                      <Typography key={index} color="textPrimary">
-                        <Link
-                          className="crumb-link"
-                          to={currentLink}
-                          color={isLastCrumb ? "textPrimary" : "inherit"}
-                        >
-                          {crumb.replace(/-/g, "  ")}
-                        </Link>
-                        {index === array.length - 1 && !isLastCrumb && (
-                          <ExpandMoreIcon fontSize="small" />
-                        )}
-                      </Typography>
-                    );
-                })}
+
+              {crumbs.map((crumb, index, array) => {
+                const currentLink = `/${array.slice(0, index + 1).join('/')}`;
+                const isLastCrumb = index === array.length - 1;
+                const isCrumb = array.filter((crumb) => crumb !== 'home');
+                return (
+
+                  <Typography key={index} color="textPrimary">
+                    <Link
+                      className="crumb-link"
+                      to={currentLink}
+                      color={isLastCrumb ? "textPrimary" : "inherit"}
+                    >
+                      {crumb.replace(/-/g, "  ")}
+                    </Link>
+                    {index === array.length - 1 && !isLastCrumb && (
+                      <ExpandMoreIcon fontSize="small" />
+                    )}
+
+                  </Typography>
+                );
+
+              })}
             </Breadcrumbs>
+          </div>
         </div>
-    );
+        <div class="col-2">
+          <div className="download-container">
+            <div className="download-bar" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              {isHovered ? (
+                <div className="download-menu-container">
+                    <button className="download-button" onClick={handleDownloadIos}>iOS</button>
+                    <button className="download-button" onClick={handleDownloadAndroid}>Android</button>
+                </div>
+              ) : (
+                <p>Download</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default BreadCrumb;
