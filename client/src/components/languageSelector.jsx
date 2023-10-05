5f52ea0d367c7a587c344ac3bc21fd17b8485e2b
Manan Santoki <manansantoki1702@gmail.com>
2023-10-05 11:48:30 +0530

    improve language selector

--- client/src/components/languageSelector.jsx
+++ client/src/components/languageSelector.jsx
@@ -1,9 +1,9 @@
-// LanguageSelector.js
-
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import lang from '../assets/images/languagess.png';
 import { setLanguage } from '../redux/slices/languageSlice';
+
+// Import your language flag image if needed
+ import lang from '../assets/images/languagess.png';
 
 function LanguageSelector() {
   const dispatch = useDispatch();
@@ -22,21 +22,22 @@ function LanguageSelector() {
 
   return (
     <div className="custom-select">
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={lang} alt="lang" width="20" height="20" />
-        <label htmlFor="languageSelector"></label>
-        <select
-          id="languageSelector"
-          value={currentLanguage}
-          onChange={handleLanguageChange}
-        >
-          {languages.map((language) => (
-            <option key={language.code} value={language.code}>
-              {language.name} {currentLanguage === language.code ? '✓' : ''}
-            </option>
-          ))}
-        </select>
-      </div>
+      {/* You can add the language flag image here */}
+      <img src={lang} alt="Language" width="20" height="20" />
+
+      
+      <select
+        id="languageSelector"
+        value={currentLanguage}
+        onChange={handleLanguageChange}
+        className="custom-select-dropdown"
+      >
+        {languages.map((language) => (
+          <option key={language.code} value={language.code}>
+            {language.name} {currentLanguage === language.code ? '✓' : ''}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
