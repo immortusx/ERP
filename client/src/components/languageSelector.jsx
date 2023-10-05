b147b7228bf5138bd9ba12a38ad9744f882f1d6f
Manan Santoki <manansantoki1702@gmail.com>
2023-10-05 10:57:30 +0530

    checkmark language selector

--- client/src/components/languageSelector.jsx
+++ client/src/components/languageSelector.jsx
@@ -1,34 +1,42 @@
 // LanguageSelector.js
 
 import React from 'react';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import lang from '../assets/images/languagess.png';
 import { setLanguage } from '../redux/slices/languageSlice';
-
 
 function LanguageSelector() {
   const dispatch = useDispatch();
   const currentLanguage = useSelector((state) => state.language.language);
 
-  const handleLanguageChange = (e) => {
+  const languages = [
+    { code: 'en', name: 'English' },
+    { code: 'gj', name: 'ગુજરાતી' },
+    // Add more languages as needed
+  ];
 
-    const selectedLanguage = e.target.value;
-    dispatch(setLanguage(selectedLanguage));
+  const handleLanguageChange = (event) => {
+    const selectedValue = event.target.value;
+    dispatch(setLanguage(selectedValue));
   };
-
-  useEffect(() => {
-    console.log(currentLanguage, "dfjjjjjjjjjjjuhl")
-  }, [currentLanguage])
 
   return (
     <div className="custom-select">
-      <img src={lang} alt="lang" height={20} width={20} />
-      <select onChange={handleLanguageChange} value={currentLanguage}>
-        <option value="en">English</option>
-        <option value="gj">ગુજરાતી</option>
-        {/* Add more language options as needed */}
-      </select>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={lang} alt="lang" width="20" height="20" />
+        <label htmlFor="languageSelector"></label>
+        <select
+          id="languageSelector"
+          value={currentLanguage}
+          onChange={handleLanguageChange}
+        >
+          {languages.map((language) => (
+            <option key={language.code} value={language.code}>
+              {language.name} {currentLanguage === language.code ? '✓' : ''}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
