113f466fc044f21119c02ef1e858e1caefac21d0
Mukesh Bhuriya <>
2023-10-06 16:13:48 +0530

    enquiry home ui smoothing

---
+++ Vehicle_app/src/components/subCom/SimpleAlert.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Modal, View, Text, Button, StyleSheet } from 'react-native';
+
+const SimpleAlert = ({ isVisible, message, onClose }) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text>{message}</Text>
+          <Button title="Close" onPress={onClose} />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+});
+
+export default SimpleAlert;
