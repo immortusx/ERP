170d06b2418f44066a6835ea723fa8fce9c9f248
karthikv-benagon <127193097+karthikv-benagon@users.noreply.github.com>
2023-10-16 17:44:35 +0530

    dashboard_data

--- client/src/components/EnquiryCharts.jsx
+++ client/src/components/EnquiryCharts.jsx
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from "react";
-import Chart from "chart.js/auto";
+import React, { useEffect, useState, useRef } from "react";
+import { Chart } from "chart.js/auto";
 import axios from "axios";
 
 const EnquiryCharts = () => {
-  const [resultData, setResultData] = useState([]);
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: "",
-        backgroundColor: "",
-        data: [],
-      },
-      {
-        label: "",
-        backgroundColor: "",
-        data: [],
-      },
-    ],
-  });
+  const [monthlyData, setMonthlyData] = useState([]);
+  const [dailyData, setDailyData] = useState([]);
+  const [chartData, setChartData] = useState(null);
+
+  const chartRef = useRef(null);
+
   useEffect(() => {
-    const getChartsData = async () => {
+    const getMonthlyData = async () => {
       const url = `${process.env.REACT_APP_NODE_URL}/api/enquiry/get-total-enquiry-booking`;
       const config = {
         headers: {
@@ -29,43 +19,101 @@ const EnquiryCharts = () => {
       };
       await axios.get(url, config).then((response) => {
         if (response.data && response.data.isSuccess) {
-          // console.log(response.data.result);
-          setResultData(response.data.result);
+          setMonthlyData(response.data.result);
         }
       });
     };
-    getChartsData();
+    getMonthlyData();
   }, []);
-  useEffect(() => {
-    if (resultData.length > 0) {
-      const updatedLables = resultData.map((item) => item.month);
-      const updateTotalEnquiry = resultData.map((item) => item.totalEnquiry);
-      const updateTotalBooking = resultData.map((item) => item.totalbooking);
-      setChartData((preData) => ({
-        ...preData,
-        labels: updatedLables,
-        datasets: [
-          {
-            label: "Enquiries",
-            backgroundColor: "rgba(54, 162, 235, 0.5)",
-            data: updateTotalEnquiry,
-          },
-          {
-            label: "Booking",
-            backgroundColor: "rgba(75, 192, 192, 0.5)",
-            data: updateTotalBooking,
-          },
-        ],
-      }));
-    }
-  }, [resultData]);
 
   useEffect(() => {
-    if (chartData.labels.length > 0) {
+    const getDailyData = async () => {
+      const url = `${process.env.REACT_APP_NODE_URL}/api/enquiry/get-todays-enquiry-booking`;
+      const config = {
+        headers: {
+          token: localStorage.getItem("rbacToken"),
+        },
+      };
+      axios.get(url, config).then((response) => {
+        if (response.data && response.data.isSuccess) {
+          setDailyData(response.data.result);
+        }
+      });
+    };
+    getDailyData();
+  }, []);
+
+  useEffect(() => {
+    if (monthlyData.length > 0 || dailyData.length > 0) {
+      const labels = [];
+      const monthlyEnquiries = [];
+      const monthlyBookings = [];
+      const dailyEnquiries = [];
+      const dailyBookings = [];
+
+      monthlyData.forEach((item) => {
+        labels.push(item.month);
+        monthlyEnquiries.push(item.totalEnquiry);
+        monthlyBookings.push(item.totalbooking);
+      });
+
+      dailyData.forEach((item) => {
+        labels.push(item.day);
+        dailyEnquiries.push(item.totalEnquiry);
+        dailyBookings.push(item.totalbooking);
+      });
+
+      setChartData({
+        labels: labels,
+        datasets: [
+          {
+            label: "Monthly Enquiries",
+            backgroundColor: "rgba(54, 162, 235, 0.5)",
+            data: [
+              ...monthlyEnquiries,
+              ...new Array(dailyData.length).fill(null),
+            ],
+          },
+          {
+            label: "Monthly Bookings",
+            backgroundColor: "rgba(75, 192, 192, 0.5)",
+            data: [
+              ...monthlyBookings,
+              ...new Array(dailyData.length).fill(null),
+            ],
+          },
+          {
+            label: "Todays Enquiries",
+            backgroundColor: "rgba(255, 99, 71, 0.6)",
+            data: [
+              ...new Array(monthlyData.length).fill(null),
+              ...dailyEnquiries,
+            ],
+          },
+          {
+            label: "Todays Bookings",
+            backgroundColor: "rgba(80, 112, 120, 5.3)",
+            data: [
+              ...new Array(monthlyData.length).fill(null),
+              ...dailyBookings,
+            ],
+          },
+        ],
+      });
+    }
+  }, [monthlyData, dailyData]);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+  }, [chartData]);
+
+  useEffect(() => {
+    if (chartData) {
       const ctx = document
         .getElementById("enquiryBookingChart")
         .getContext("2d");
-
       const config = {
         type: "bar",
         data: chartData,
@@ -78,7 +126,7 @@ const EnquiryCharts = () => {
         },
       };
 
-      new Chart(ctx, config);
+      chartRef.current = new Chart(ctx, config);
     }
   }, [chartData]);
 
