ad65793c936fcf1e06c0c4ae206f3d980ee6743a
Manan Santoki <manansantoki1702@gmail.com>
2023-10-30 10:52:18 +0530

    fix issue for build failing
    fix issue where package name and app name containing blank spaces or hypen fails android app build

--- Vehicle_app/app-name.sh
+++ Vehicle_app/app-name.sh
@@ -9,14 +9,18 @@ fi
 # New app name is the first argument provided.
 NEW_APP_NAME="$1"
 
+# Edit strings.xml
+sed -i "s#<string name=\"app_name\">.*</string>#<string name=\"app_name\">$NEW_APP_NAME</string>#" android/app/src/main/res/values/strings.xml
+
+# Replace white spaces and hyphens with underscores in NEW_APP_NAME
+NEW_APP_NAME=$(echo "$NEW_APP_NAME" | sed -e 's/ /_/g' -e 's/-/_/g')
+
 # Edit MainActivity.java
 sed -i "16s/.*/    return \"$NEW_APP_NAME\";/" android/app/src/main/java/com/vehicle_crm/MainActivity.java
 
 # Edit settings.gradle
 sed -i "1s/.*/rootProject.name = '$NEW_APP_NAME'/" android/settings.gradle
 
-# Edit strings.xml
-sed -i "s#<string name=\"app_name\">.*</string>#<string name=\"app_name\">$NEW_APP_NAME</string>#" android/app/src/main/res/values/strings.xml
 
 # Edit app.json
 { echo "{"; echo "  \"name\": \"$NEW_APP_NAME\","; echo "  \"displayName\": \"$NEW_APP_NAME\""; echo "}"; } > app.json
