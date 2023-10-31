7ff99d9da4284614062dd67aec90584cf6c6551d
Laxmi Chaudhari <127193097+karthikv-benagon@users.noreply.github.com>
2023-10-31 17:47:38 +0530

    agency

--- Vehicle_app/src/redux/slice/agencyDataSlice.js
+++ Vehicle_app/src/redux/slice/agencyDataSlice.js
@@ -1,61 +1,65 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import axios from 'axios';
-import {API_URL} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {API_URL} from '@env';
 
 const initialState = {
   agencyDataState: {
     isSuccess: false,
-    isError: false,
     isFetching: false,
-    result: null, // Modify the result to be an object
+    isError: false,
+    result: null,
   },
 };
 
-export const agencyDb = createAsyncThunk('agencyDb/agencySlice', async () => {
-  const url = `${API_URL}/api/get-agencylogo`; // Use the correct endpoint
-  const token = await AsyncStorage.getItem('rbacToken');
-  const config = {
-    headers: {
-      token: token ? token : '',
-    },
-  };
-
-  try {
-    const response = await axios.get(url,config);
-    console.log(response.data,"%%%%%%%%%%%%%%%%%%%%%%%%");
-    return response.data; // Return the data directly
-  } catch (error) {
-    throw error;
-  }
-});
-
-const agencyDataSlice = createSlice({
+export const getAgencyData = createAsyncThunk(
+  'getAgencyData/getAgencySlice',
+  async () => {
+    const url = `${API_URL}/api/agency/get-agencylogo`;
+    console.log(url, 'agencyData');
+    try {
+      const token = await AsyncStorage.getItem('rbacToken');
+      const config = {
+        headers: {
+          token: token || '',
+        },
+      };
+      const response = await axios.get(url, config);
+      console.log(response.data.result, 'agencyData');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+);
+const getAgencySlice = createSlice({
   name: 'agencyDataState',
   initialState,
   reducers: {
-    clearagencyDataState: state => {
-      state.agencyDataState.isError = false;
-      state.agencyDataState.isSuccess = false;
-      state.agencyDataState.isFetching = false;
-      state.agencyDataState.result = null; // Modify result to be null
+    clearAgencyDataState: state => {
+      (state.agencyDataState.isError = false),
+        (state.agencyDataState.isFetching = false),
+        (state.agencyDataState.isSuccess = false),
+        (state.agencyDataState.result = null);
     },
   },
   extraReducers: builder => {
-    builder.addCase(agencyDb.pending, state => {
-      state.agencyDataState.isFetching = true;
+    builder.addCase(getAgencyData.pending, state => {
+      (state.agencyDataState.isFetching = true),
+        (state.agencyDataState.isError = false);
     });
-    builder.addCase(agencyDb.fulfilled, (state, action) => {
-      state.agencyDataState.isFetching = false;
-      state.agencyDataState.isSuccess = true;
-      state.agencyDataState.result = action.payload;
+    builder.addCase(getAgencyData.fulfilled, (state, action) => {
+      (state.agencyDataState.isFetching = false),
+        (state.agencyDataState.isError = false),
+        (state.agencyDataState.isSuccess = true),
+        (state.agencyDataState.result = action.payload);
     });
-    builder.addCase(agencyDb.rejected, (state, action) => {
-      state.agencyDataState.isFetching = false;
-      state.agencyDataState.isError = true;
+    builder.addCase(getAgencyData.rejected, state => {
+      (state.agencyDataState.isError = true),
+        (state.agencyDataState.isFetching = false);
     });
   },
 });
 
-export const {clearagencyDataState} = agencyDataSlice.actions;
-export default agencyDataSlice.reducer;
+export const {clearAgencyDataState} = getAgencySlice.actions;
+export default getAgencySlice.reducer;
