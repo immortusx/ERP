53eb9d5d893b587e7152f8286a39f3edd92c3d99
Laxmi Chaudhari <127193097+karthikv-benagon@users.noreply.github.com>
2023-11-04 17:07:23 +0530

    send_mail

--- server/Routes/mailRoute.js
+++ server/Routes/mailRoute.js
@@ -11,42 +11,57 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: "albynova67@gmail.com",
-    pass: "goju bzmt djxy gvir",
+    user: "sales.balkrushna@gmail.com",
+    pass: "jnouvesshyjdvaui",
   },
 });
-// Define a cron job to run daily at 5:23 PM (adjust the schedule as needed)
-cron.schedule("33 17 * * *", async () => {
+cron.schedule("0 21 * * *", async () => {
   try {
-    const url = `CALL sp_get_work_report_for_currentdate()`;
+    const workReportQuery = "CALL sp_get_work_report_for_currentdate()";
 
-    db.query(url, async (err, result) => {
-      if (err) {
-        console.error(err);
+    db.query(workReportQuery, async (workReportErr, workReportResult) => {
+      if (workReportErr) {
+        console.error(workReportErr);
       } else {
-        const data = result[0];
+        const workReportData = workReportResult[0];
 
-        const filename = "work_report.csv";
-        const fileStream = fs.createWriteStream(filename);
+        const workReportFilename = "work_report.csv";
+        const workReportStream = fs.createWriteStream(workReportFilename);
 
         fastcsv
-          .write(data, { headers: true })
+          .write(workReportData, { headers: true })
           .on("finish", () => {
-            console.log("CSV file created successfully.");
-         transporter.sendMail({
-           from: "albynova67@gmail.com", // Your email address
-           to: "laxmichaudhari203@gmail.com", // Recipient's email address
-           subject: "Work Report", // Email subject
-           text: "Please find the attached work report.", // Email text
-           attachments: [
-             {
-               filename: "work_report.csv",
-               content: fs.createReadStream(filename), // Attach the CSV file
-             },
-           ],
-         });
+            console.log("Work report CSV file created successfully.");
+
+            const superAdminEmailQuery =
+              "SELECT email FROM users WHERE id = 1";
+            db.query(
+              superAdminEmailQuery,
+              async (superAdminEmailErr, superAdminEmailResult) => {
+                if (superAdminEmailErr) {
+                  console.error(superAdminEmailErr);
+                } else {
+                  const Email = superAdminEmailResult[0].email;
+                  console.log(Email, "superAdminEmail");
+
+                  transporter.sendMail({
+                    from: "sales.balkrushna@gmail.com",
+                    to: Email,
+                    cc: "info@balkrushna.com",
+                    subject: "Work Report",
+                    text: "Please find the attached work report.",
+                    attachments: [
+                      {
+                        filename: "work_report.csv",
+                        content: fs.createReadStream(workReportFilename),
+                      },
+                    ],
+                  });
+                }
+              }
+            );
           })
-          .pipe(fileStream);
+          .pipe(workReportStream);
       }
     });
   } catch (error) {
