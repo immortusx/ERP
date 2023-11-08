d8ce5104a33a5e386413c7f81d401b903d70489e
parthdave <parth.dave@balkrushna.com>
2023-11-08 17:19:24 +0530

    sent Whatsapp Message to customer and SSP

--- server/Routes/whatsappRoutes.js
+++ server/Routes/whatsappRoutes.js
@@ -1,39 +1,67 @@
 const express = require('express');
 const request = require('request');
 const { tokenCheck } = require("../Auth/TokenCheck");
+const fs = require("fs");
+const fastcsv = require("fast-csv");
+const { db } = require("../Database/dbConfig");
+const cron = require("node-cron");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
+
 router.post('/send-whatsapp', tokenCheck, (req, res) => {
-console.log(req.body, 'erlelr')
-    const phoneNumber = '916355193009';
+    const whatsappMessage = req.body.whatsapp_message;
+    let customerNumber = req.body.c_phone_number;
 
-    const options = {
-        method: 'POST',
-        url: 'https://whats-api.rcsoft.in/api/create-message',
-        headers: {},
-        formData: {
-            appkey: '86906369-e91b-4772-8776-b49ce3778fd5',
-            authkey: 'qZNwxpV01C58oQa1FluB24YUv3p61zuZf0Re0L795Z5UWrW3ao',
-            to: phoneNumber,
-            message: 'Welcome to New Keshav Tractors!',
+    const userId = req.myData.userId;
 
-        }
-    };
-
-    request(options, function (error, response, body) {
-        if (error) {
-            console.error('Error sending message to', phoneNumber, error);
-            res.status(500).send('Failed to send WhatsApp message');
+    const salesPersonId = userId;
+    
+    const salesPersonQuery = 'SELECT phone_number FROM users WHERE id = 75';
+    db.query(salesPersonQuery, async (err, result) => {
+        if (err) {
+            console.log({ isSuccess: false, result: 'error' })
+            // res.send({ isSuccess: false, result: 'error' })
         } else {
-            if (response.statusCode === 200) {
-                console.log('Message sent to', phoneNumber, body);
-                res.send('WhatsApp message sent to ' + phoneNumber);
-            } else {
-                console.error('Failed to send message to', phoneNumber, 'Status Code:', response.statusCode, 'Response:', body);
-                res.status(response.statusCode).send('Failed to send WhatsApp message');
-            }
+            console.log({ isSuccess: true, result: result })
+            console.log(result, 'sale phone nmube')
+            const phoneNumbers = [];
+            let salesPersonWhatsAppNumber = result[0].phone_number;
+            console.log(phoneNumbers, 'ponenr')
+            phoneNumbers.push(customerNumber, salesPersonWhatsAppNumber)
+            phoneNumbers.forEach(function (phoneNumber) {
+                var options = {
+                    method: 'POST',
+                    url: 'https://whats-api.rcsoft.in/api/create-message',
+                    headers: {},
+                    formData: {
+                        appkey: '86906369-e91b-4772-8776-b49ce3778fd5',
+                        authkey: 'qZNwxpV01C58oQa1FluB24YUv3p61zuZf0Re0L795Z5UWrW3ao',
+                        to: phoneNumber,
+                        message: whatsappMessage,
+                        file: 'https://www.africau.edu/images/default/sample.pdf'
+                    }
+                };
+
+                request(options, function (error, response, body) {
+                    if (error) {
+                        console.error('Error sending message to', phoneNumber, error);
+                    } else {
+                        if (response.statusCode === 200) {
+                            console.log('Message sent to', phoneNumber, body);
+                        } else {
+                            console.error('Failed to send message to', phoneNumber, 'Status Code:', response.statusCode, 'Response:', body);
+                        }
+                    }
+                });
+            });
+            // phoneNumbers.push(cust)
+            // res.send({isSuccess: false, result: 'error'})
+
         }
-    });
+    })
+    res.send({ isSuccess: true, result: 'Sent' })
+
 });
 
 module.exports = router;
