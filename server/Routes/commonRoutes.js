b32084cccfc9484ce1b3ab6f7beb03306a6ea32a
Laxmi Vishwakarma <laxmi.vishwakarma@balkrushna.com>
2023-11-22 18:24:44 +0530

    assign task date issue fixed

--- server/Routes/commonRoutes.js
+++ server/Routes/commonRoutes.js
@@ -182,8 +182,8 @@ router.post("/addtask-data", tokenCheck, async (req, res) => {
     const tasktypePeriod = req.body.tasktimePeriod;
     const taskStatus = req.body.taskStatus;
     const taskCategory = req.body.taskCategory;
-    const startDate = req.body.startDate.split("T")[0];
-    const endDate = req.body.endDate.split("T")[0];
+    const startDate = req.body.startDate;
+    const endDate = req.body.endDate;
 
     const url = `INSERT INTO addtask_data (employee, tasktype, task, taskcount, startdate, enddate,tasktime_period,task_status,category_name) VALUES (?,?,?,?,?,?,?,?,?)`;
 
