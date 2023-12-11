cdabb8c96231221c78cbc3d3feb8458f8c0cf157
Mukesh Bhuriya <mukesh.bhuriya@balkrushna.com>
2023-12-11 09:57:48 +0530

    add regards message in messaging helper

--- server/Utils/MessagingHelpers.js
+++ server/Utils/MessagingHelpers.js
@@ -2,9 +2,14 @@ const request = require("request");
 
 const appkey = process.env.appkey;
 const authkey = process.env.authkey;
+const { db } = require("../Database/dbConfig");
 
 const InstantMessagingUtils = async (chatPayloads) => {
   const { phoneNumbers, message, files } = chatPayloads;
+
+  const regardsMessage = (await getRegardsMessages().catch(() => null)) || "From Our Teams";
+  const newMessage = `${message}\n\n*Best regards,*\n${regardsMessage}`;
+
   const formattedPhoneNumbers = phoneNumbers.map((phoneNumber) => {
     // Check if the phone number starts with "91", if not, add it
     return String(phoneNumber).startsWith("91") ? phoneNumber : `91${phoneNumber}`;
@@ -19,7 +24,7 @@ const InstantMessagingUtils = async (chatPayloads) => {
           appkey: appkey,
           authkey: authkey,
           to: phoneNumber,
-          message: message,
+          message: newMessage,
           // file: "https://crm.balkrushna.com/api/upload/1701076738457__upload_1696416072385_download%20(3).jpg",
         },
       };
@@ -53,7 +58,7 @@ const InstantMessagingUtils = async (chatPayloads) => {
           appkey: appkey,
           authkey: authkey,
           to: phoneNumber,
-          message: message,
+          message: newMessage,
           file: files,
         },
       };
@@ -80,4 +85,33 @@ const InstantMessagingUtils = async (chatPayloads) => {
   }
 };
 
+const getRegardsMessages = async () => {
+  try {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT * FROM configuration WHERE setting = 'agency' AND key_name = 'name'`,
+        (error, dataResults) => {
+          if (error) {
+            console.log({ isSuccess: false, result: "error" });
+            resolve(null);
+          } else {
+            console.log(dataResults, "dataResults");
+            if (dataResults && dataResults.length > 0) {
+              const rowDataPacket = dataResults[0];
+              const regardsMessage = rowDataPacket.value;
+              resolve(regardsMessage);
+              console.log(regardsMessage, "read");
+            } else {
+              console.log({ isSuccess: false, result: "No data found" });
+              resolve(null);
+            }
+          }
+        }
+      );
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 module.exports = { InstantMessagingUtils };
